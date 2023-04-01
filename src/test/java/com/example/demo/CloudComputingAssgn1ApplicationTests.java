05cab35e83210253fee4f4553cc64a36d6ad4f20
Ruchi Upadhyay <upadhyay.ruc@northeastern.edu>
2023-03-31 21:11:39 -0400

    correcting wrong merge

---
+++ src/test/java/com/example/demo/CloudComputingAssgn1ApplicationTests.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.controllers.AppHealthController;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CloudComputingAssgn1ApplicationTests {
+	
+	@Autowired
+	private AppHealthController healthController;
+	
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	public void checkHealth() {
+		
+		assertEquals("", healthController.checkHealth());
+	}
+}
