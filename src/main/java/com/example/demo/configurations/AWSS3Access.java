951ec3aafe7c9f0d2e648239f80a619e68d7327c
Ruchi Upadhyay <upadhyay.ruc@northeastern.edu>
2023-03-31 21:11:39 -0400

    correcting wrong merge

---
+++ src/main/java/com/example/demo/configurations/AWSS3Access.java
@@ -0,0 +1,21 @@
+package com.example.demo.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSS3Access {
+
+
+	/*
+	 * @Bean AmazonS3 getAmazonS3Client() { final BasicAWSCredentials
+	 * basicAWSCredentials = new BasicAWSCredentials(accessKeyId, accessKeySecret);
+	 * 
+	 * System.out.println("AccessID: " + accessKeyId + " SECRET: " + accessKeySecret
+	 * + " REGION: " + s3RegionName);
+	 * 
+	 * // Get Amazon S3 client and return the S3 client object return
+	 * AmazonS3ClientBuilder .standard() .withCredentials(new
+	 * AWSStaticCredentialsProvider(basicAWSCredentials)) .withRegion(s3RegionName)
+	 * .build(); }
+	 */
+}
